1df9fcd1839344b06dacc587e5337467f5a47d3e
Deepak Mohan <depakmohan93@gmail.com>
2026-06-15 11:55:39 +0530

    Add second GTM container (GTM-T7DVHD77)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-NGXPZKCD');`}
         </Script>
         {/* End Google Tag Manager */}
+        {/* Google Tag Manager */}
+        <Script id="gtm-head-2" strategy="beforeInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-T7DVHD77');`}
+        </Script>
+        {/* End Google Tag Manager */}
         <Script
           id="hs-script-loader"
           src="//js-na2.hs-scripts.com/246155920.js"
@@ -35,6 +40,16 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-NGXPZKCD"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-T7DVHD77"
             height="0"
             width="0"
             style={{ display: 'none', visibility: 'hidden' }}
